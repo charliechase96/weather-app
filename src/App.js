018c8f42fc910ab2ca2b604d80283a502b974a72
Charlie Gray <charliechasegray@gmail.com>
2023-12-18 13:02:09 -0800

    build search and weather components

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Search from './Components/Search';
+import Weather from './Components/Weather';
+
+function App() {
+  return (
+    <div className='display'>
+      <Search />
+      <Weather />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Weather.js
@@ -0,0 +1,29 @@
+import React, {useState, useEffect} from "react";
+
+function Weather() {
+    const [weatherData, setWeatherData] = useState({});
+
+    useEffect(() => {
+        fetch(`https://api.weatherapi.com/v1/forecast.json?key=9ffefacc209c4dc9803111103231912&q=85004&days=3&aqi=yes&alerts=yes`)
+        .then(response => response.json())
+        .then(data => setWeatherData(data)
+    )}, []);
+
+    console.log(weatherData.current.condition.text)
+
+    return (
+        <div>
+            <ul>
+                <li>Location: {weatherData.location.name}, {weatherData.location.region}</li>
+                <li>Conditions: {weatherData.current.condition.text}</li>
+                <li>Current Temp: {weatherData.current.temp_f}°F</li>
+                <li>Today's High</li>
+                <li>Humidity: {weatherData.current.humidity}%</li>
+                <li></li>
+            </ul> 
+        </div>
+        
+    )
+}
+
+export default Weather;
